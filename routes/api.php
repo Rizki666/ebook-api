fe12da68caafbc6840b9fb8fe45b761ed378ca3f
Rizki666 <70094809+Rizki666@users.noreply.github.com>
2022-10-26 11:00:47 +0700

    challenge author

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,3 +22,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/me',[AuthController::class, 'me']);
+
+Route::resource('books', BookController::class)->except(
+    ['create','edit']
+);
+
+Route::resource('authors', AuthorController::class)->except(
+    ['create','edit']
+);
